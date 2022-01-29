f70003c1f86ad8321249e025db3db6534ce5001b
andna5980 <andy5980@hotmail.com>
2022-01-29 07:17:29 +0000

    added basic stripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.contrib import messages
 from django.conf import settings
 
@@ -29,6 +29,7 @@ def checkout(request):
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
+        
         if order_form.is_valid():
             order = order_form.save()
             for item_id, item_data in bag.items():
