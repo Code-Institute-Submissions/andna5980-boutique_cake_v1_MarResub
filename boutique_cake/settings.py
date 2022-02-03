cb7ec6de77291c5595c663402bb3a0a15c4b1b83
andna5980 <andy5980@hotmail.com>
2022-02-03 09:40:58 +0000

    Remove secret key and set debug

--- boutique_cake/settings.py
+++ boutique_cake/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7&f%rgch7#+0o*nm0hb3!(-+3t2yy*neujm+2@a7h-tp9)20=l'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.emviron
+
 
 ALLOWED_HOSTS = ['andna5980-boutiquecake-v1.herokuapp.com', 'localhost']
 
